fa00245a3ea6b50fa8bdc9844fa5434c7cbfc66b
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-06-24 10:28:38 +0200

    feat: Add Repository file to make http request

---
+++ lib/repository/schedule_repository.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:harperdb_hashnode_hackathon/models/failure_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:harperdb_hashnode_hackathon/models/post_model.dart';
+
+class ScheduleRepository {
+
+  static String _baseUrl = '${dotenv.env['HARPER_DB_URL']}';
+  final http.Client _client;
+
+  ScheduleRepository({http.Client? client}) : _client = client ?? http.Client();
+
+  void dispose() {
+    _client.close();
+  }
+
+  Future<List<Post>> getPosts() async {
+    try {
+      final response = await _client.post(
+        Uri.parse(_baseUrl),
+        headers: {
+          HttpHeaders.authorizationHeader:
+              'Basic ${dotenv.env['HARPER_DB_SECRET_KEY']}',
+          HttpHeaders.contentTypeHeader: 'application/json'
+        },
+        body: json.encode({"operation": "sql", "sql": "SELECT * FROM schedule.post"}) ,
+      );
+
+      if (response.statusCode == 200) {
+        return (jsonDecode(response.body) as List).map((e) => Post.fromJson(e)).toList()
+        ..sort((a, b) => b.postDate.compareTo(a.postDate));
+      } else {
+        throw const Failure(message: 'Something went wrong! - 1');
+      }
+    } catch (_) {
+      throw const Failure(message: 'Something went wrong! - 2');
+    }
+  }
+}
