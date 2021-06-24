faea9499396c57527e657898e3ea5f0fc9686947
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-06-24 10:29:11 +0200

    feat: Add first version of the UI

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter/material.dart';
+import 'package:harperdb_hashnode_hackathon/models/failure_model.dart';
+import 'package:harperdb_hashnode_hackathon/models/post_model.dart';
+import 'package:harperdb_hashnode_hackathon/repository/schedule_repository.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  late Future<List<Post>> _futurePosts;
+
+  @override
+  void initState() {
+    super.initState();
+    _futurePosts = ScheduleRepository().getPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _futurePosts = ScheduleRepository().getPosts();
+          setState(() {});
+        },
+        child: FutureBuilder<List<Post>>(
+          future: _futurePosts,
+          builder: (context, snapshot) {
+            print(snapshot);
+            if (snapshot.hasData) {
+              final items = snapshot.data!;
+              return ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+
+                  final item = items[index];
+                  return Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: ListTile(
+                      title: Text('${item.title}'),
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              // Show failure error message.
+              final failure = snapshot.error as Failure;
+              return Center(child: Text(failure.message));
+            }
+            // Show a loading spinner.
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
+      ),
+    );
+  }
+}
