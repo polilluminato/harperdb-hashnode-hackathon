94560bb0f91b90521919c502e485641c61ae30ee
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-06-26 15:57:35 +0200

    feat: Change post row UI

--- lib/ui/post_row.dart
+++ lib/ui/post_row.dart
@@ -22,16 +22,68 @@ class PostRow extends StatelessWidget {
         );
       },
       child: Container(
-        margin: const EdgeInsets.all(8.0),
-        child: ListTile(
-          title: Text('${post.title}'),
-          subtitle:
-              Text('${DateFormat('dd MMMM y HH:mm').format(post.postDate)}'),
-          leading: Image.asset(
-            "assets/logo/${post.platform}.png",
-            width: 32.0,
-          ),
-          trailing: Icon(Icons.navigate_next),
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 24),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  '${DateFormat('dd').format(post.postDate)}',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '${DateFormat('MMM').format(post.postDate)}',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Image.asset(
+                  "assets/logo/${post.platform}.png",
+                  width: 24,
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 24,
+            ),
+            Expanded(
+                child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${post.title}',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    '${post.text}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    '${post.link}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ))
+          ],
         ),
       ),
     );
